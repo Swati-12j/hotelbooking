709f2c576ae5efcef9a7b7d125c77c1a3741d328
Swati <swatiolla0001@gmail.com>
2024-11-10 12:53:07 +0530

    Your commit message here

--- admin/hello/hello0.php
+++
@@ -1,1 +0,0 @@
-this is php_check_syntax
--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -3,12 +3,14 @@
 //frontend purpose data
 define('SITE_URL','http://127.0.0.1/phpca-main/');
 define('ABOUT_IMG_PATH',SITE_URL.'images/about/');
+define('CAROUSEL_IMG_PATH', SITE_URL .'images/carousel/');
 
 //backend upload process needs this data
 
 
 define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/phpca-main/images/');
 define('ABOUT_FOLDER', 'about/');
+define('CAROUSEL_FOLDER', 'carousel/');
 
 function adminLogin()
 {
